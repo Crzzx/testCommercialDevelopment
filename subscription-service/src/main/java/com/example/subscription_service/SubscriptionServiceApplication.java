4329dacbecf906455d7edc4e90452b019eb95f78
Leonid <leobalashiv@gmail.com>
2025-07-12 23:27:14 +0500

    first commit

---
+++ subscription-service/src/main/java/com/example/subscription_service/SubscriptionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.subscription_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SubscriptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionServiceApplication.class, args);
+	}
+
+}
